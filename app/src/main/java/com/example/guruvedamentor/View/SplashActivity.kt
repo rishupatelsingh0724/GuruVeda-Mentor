b37f135548521779f7986cacb6eb26d3b3c8b4bf
Rishu Patel <rishupatelsingh@gmail.com>
2025-03-27 14:15:45 +0530

    Some changes in SplashActivity

--- app/src/main/java/com/example/guruvedamentor/View/SplashActivity.kt
+++ app/src/main/java/com/example/guruvedamentor/View/SplashActivity.kt
@@ -1,17 +1,31 @@
 package com.example.guruvedamentor.View
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import com.example.guruvedamentor.Auth.LoginActivity
+import com.example.guruvedamentor.MainActivity
 import com.example.guruvedamentor.R
+import com.google.firebase.auth.FirebaseAuth
 
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+    private val auth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
-
+        Handler(Looper.getMainLooper()).postDelayed({
+            if (auth.currentUser!!.uid != null) {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            } else {
+                startActivity(Intent(this, LoginActivity::class.java))
+            }
+        }, 1000)
     }
 }
